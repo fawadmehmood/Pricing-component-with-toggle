ed916a2c59f62840ae714180d676713e1834e353
fawadmehmood <fawadmehmood10@gmail.com>
2020-11-25 17:21:14 +0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const toggle =  document.querySelector(".toogle");
+const circle = document.querySelector(".circle");
+const price = document.querySelectorAll(".p_price");
+const annualPrices = [119.99, 299.99, 399.99];
+const monthlyPrices = [19.99, 29.99, 39.99];
+
+toggle.addEventListener("click", ()=>{
+        circle.classList.toggle("move");
+        if(circle.classList.contains("move")){
+        Array.from(price).forEach((element, index) => {
+                element.innerHTML = monthlyPrices[index];
+        });
+}
+else{
+        Array.from(price).forEach((element, index) => {
+                element.innerHTML = annualPrices[index];
+        });
+}
+});
